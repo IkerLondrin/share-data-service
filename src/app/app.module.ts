11595a2d7b8c25cad8d4f7673d7f330895385210
Iker <ikerlondrin@gmail.com>
2020-05-09 21:32:09 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComponenteAComponent } from './components/componente-a/componente-a.component';
+import { ComponenteBComponent } from './components/componente-b/componente-b.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponenteAComponent,
+    ComponenteBComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
